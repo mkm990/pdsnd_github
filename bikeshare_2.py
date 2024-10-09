d56b152d29f94b0c2514b20dc8400811ea0513fc
Mohamed almadali <moham990med@gmail.com>
2024-10-09 14:53:48 +0400

    Refactor: Simplify data loading and filtering logic in load_data

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -40,6 +40,16 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
+    # Load data into a DataFrame
+    df = pd.read_csv(CITY_DATA[city])
+
+    # Convert the Start Time column to datetime
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+
+    # Extract month and day of week from Start Time to create new columns
+    df['month'] = df['Start Time'].dt.month
+    df['day_of_week'] = df['Start Time'].dt.day_name()
+
 
 
     return df
